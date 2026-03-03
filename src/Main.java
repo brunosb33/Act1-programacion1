88851e872a4fb980e4c5c22911de97d33b7954e4
brunosb33 <168544829+brunosb33@users.noreply.github.com>
2026-03-03 20:58:51 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hola Programación I");
+        System.out.println("Hola Programación I")
 
     }
 }
